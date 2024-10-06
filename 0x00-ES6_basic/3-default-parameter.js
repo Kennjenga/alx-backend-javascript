d347c757f6b1d04966f8f8d31562e63538bf25be
kennjenga <kinyagia10@gmail.com>
2024-10-06 13:15:06 +0300

    fixing errors & return an iterator to go through every employee in every department.

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,9 +1,9 @@
-//key parameters
+// key parameters
 
 export default function getSumOfHoods(
   initialNumber,
   expansion1989 = 89,
-  expansion2019 = 19
+  expansion2019 = 19,
 ) {
   return initialNumber + expansion1989 + expansion2019;
 }
