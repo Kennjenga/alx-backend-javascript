91c015446ef83385341fa553debddbfbc806a760
kennjenga <kinyagia10@gmail.com>
2024-10-07 19:50:13 +0300

    Add cleanSet function for set cleaning

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, prefix) {
+  if (!set && !(set instanceof Set)) {
+    return "";
+  }
+
+  //   If prefix is an empty string, return the set as a string
+  if (prefix === "") {
+    return "";
+  }
+
+  //   Remove the prefix from each item in the set and join them with '-'
+  //   Return the new string as the result
+  return [...set].filter((item) => item.startsWith(prefix)).join("-");
+}
