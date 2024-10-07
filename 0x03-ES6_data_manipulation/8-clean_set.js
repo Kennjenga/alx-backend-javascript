20f324662aa6c06d9ca32798557690babc95627d
kennjenga <kinyagia10@gmail.com>
2024-10-07 20:18:54 +0300

    Refactor cleanSet function to remove prefix and join items with hyphens

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,36 @@
 export default function cleanSet(set, prefix) {
-  if (!set && !(set instanceof Set)) {
+  if (!set && !prefix && !(set instanceof Set) && typeof prefix !== 'string') {
     return '';
   }
 
-  //   If prefix is an empty string, return the set as a string
-  if (prefix === '') {
-    return '';
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(prefix)) {
+      const valueSubStr = value.substring(prefix.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
   }
-
-  //   Remove the prefix from each item in the set and join them with '-'
-
-  return [...set]
-    .filter((item) => item.startsWith(prefix))
-    .map((item) => item.replace(prefix, ''))
-    .join('-');
+  return parts.join('-');
 }
+
+// export default function cleanSet(set, prefix) {
+//   if (!set && !(set instanceof Set)) {
+//     return '';
+//   }
+
+//   //   If prefix is an empty string, return the set as a string
+//   if (prefix === '') {
+//     return '';
+//   }
+
+//   //   Remove the prefix from each item in the set and join them with '-'
+
+//   return [...set]
+//     .filter((item) => item.startsWith(prefix))
+//     .map((item) => item.replace(prefix, ''))
+//     .join('-');
+// }
