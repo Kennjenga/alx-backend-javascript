545eee7ce024d8790ba94a12b46b661c8954de0f
kennjenga <kinyagia10@gmail.com>
2024-10-09 19:09:57 +0300

    Refactor cleanSet function to remove prefix and join items with hyphens

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,14 +5,36 @@ export interface Teacher {
     yearsOfExperience?: number;
     location: string;
     [index: string]: any;
-  }
+}
 
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-  };
+export interface Directors extends Teacher{
+    numberOfReports: number;
+}
+
+
+export function printTeacher(firstName: string, lastName: string): string{
+    return `${firstName[0]}. ${lastName}`;
+}
   
-console.log(teacher3);
+  export interface IsStudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  export class StudentClass implements IsStudentClass {
+    private _firstName!: string;
+    private _lastName!: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this._firstName = firstName;
+      this._lastName = lastName;
+    }
+  
+    workOnHomework(): string { 
+      return 'Currently working';
+    }
+  
+    displayName(): string{
+      return this._firstName;
+    }
+  }
